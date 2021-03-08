291034ca4e2e8ee5f306b202193dd6e724a4a08b
Ben Grant <benjamin.grantGRA0007@gmail.com>
2021-03-08 23:13:23 +1100

    Enable ssl redirect

--- crabfit-frontend/deploy.sh
+++ crabfit-frontend/deploy.sh
@@ -10,10 +10,14 @@ handlers:
 - url: /(.*\..+)$
   static_files: \1
   upload: (.*\..+)$
+  secure: always
+  redirect_http_response_code: 301
 
 - url: /.*
   static_files: index.html
   upload: index.html
+  secure: always
+  redirect_http_response_code: 301
 EOF
 
 gcloud app deploy --project=crabfit
