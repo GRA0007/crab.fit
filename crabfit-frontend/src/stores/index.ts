01a9b55b6a2feec93a5c3f2a86484dc18efdd9be
Ben Grant <benjamin.grantGRA0007@gmail.com>
2021-04-12 12:09:10 +1000

    Dark mode toggle in settings

--- crabfit-frontend/src/stores/index.ts
+++ crabfit-frontend/src/stores/index.ts
@@ -5,9 +5,11 @@ export const useSettingsStore = create(persist(
   set => ({
     weekStart: 0,
     timeFormat: '12h',
+    theme: 'System',
 
     setWeekStart: weekStart => set({ weekStart }),
     setTimeFormat: timeFormat => set({ timeFormat }),
+    setTheme: theme => set({ theme }),
   }),
   { name: 'crabfit-settings' },
 ));
