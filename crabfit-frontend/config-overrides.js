3d263580867d26c00d7794c94b96d1e98befee1c
Ben Grant <benjamin.grantGRA0007@gmail.com>
2021-06-01 04:52:01 +1000

    Override chunk names

--- crabfit-frontend/config-overrides.js
+++ crabfit-frontend/config-overrides.js
@@ -3,5 +3,9 @@ module.exports = function override(config, env) {
     ? 'static/js/[name].[contenthash].js'
     : env === 'development' && 'static/js/bundle.js';
 
+  config.output.chunkFilename = env === 'production'
+    ? 'static/js/[name].[contenthash].chunk.js'
+    : env === 'development' && 'static/js/[name].chunk.js';
+
   return config;
 }
