e94559c4f628ebe5843d5b11b56bd62ddb3bd040
Ben Grant <benjamin.grantGRA0007@gmail.com>
2021-06-19 10:32:09 +1000

    Hide extension buttons in android app

--- crabfit-frontend/src/pages/Privacy/privacyStyle.ts
+++ crabfit-frontend/src/pages/Privacy/privacyStyle.ts
@@ -7,6 +7,8 @@ export const Note = styled.p`
   padding: 12px 16px;
   margin: 16px 0;
   box-sizing: border-box;
+  font-weight: 500;
+	line-height: 1.6em;
 
   & a {
     color: ${props => props.theme.mode === 'light' ? props.theme.primaryDark : props.theme.primaryLight};
