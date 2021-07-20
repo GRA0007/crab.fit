44a6ab9c51d0839fb53ed243234d1b4d17af6b80
Ben Grant <benjamin.grantGRA0007@gmail.com>
2021-07-21 00:14:59 +1000

    Update origin trial

--- crabfit-frontend/src/pages/Home/Home.tsx
+++ crabfit-frontend/src/pages/Home/Home.tsx
@@ -224,11 +224,11 @@ const Home = ({ offline }) => {
           <h2>{t('home:about.name')}</h2>
           <Stats>
             <Stat>
-              <StatNumber>{stats.eventCount ?? '350+'}</StatNumber>
+              <StatNumber>{stats.eventCount ?? '600+'}</StatNumber>
               <StatLabel>{t('home:about.events')}</StatLabel>
             </Stat>
             <Stat>
-              <StatNumber>{stats.personCount ?? '550+'}</StatNumber>
+              <StatNumber>{stats.personCount ?? '1500+'}</StatNumber>
               <StatLabel>{t('home:about.availabilities')}</StatLabel>
             </Stat>
           </Stats>
