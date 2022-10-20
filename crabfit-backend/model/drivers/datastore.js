127e27e5698e6fc17dd4d390f8fe713d6875b6a2
TuringTux <53300311+TuringTux@users.noreply.github.com>
2022-10-20 11:06:41 +0200

    Add stub file for datastore implementation

---
+++ crabfit-backend/model/drivers/datastore.js
@@ -0,0 +1,18 @@
+/**
+ * Concrete, storage-aware implementations of the base types as defined in base.js.
+ *
+ * These implementations use Google's Datastore to store the data.
+ */
+import { BaseEvent, BasePerson, BaseStat } from "../base"
+
+export class Event extends BaseEvent {
+  // TODO
+}
+
+export class Person extends BasePerson {
+  // TODO
+}
+
+export class Stat extends BaseStat {
+  // TODO
+}
