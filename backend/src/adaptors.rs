2ba96cc9cb6a70ef9e1ecb86af45100ba4e1bfb4
Ben Grant <benjamin.grantGRA0007@gmail.com>
2023-05-15 18:33:32 +1000

    Setup features for conditional compilation

--- backend/src/adaptors.rs
+++ backend/src/adaptors.rs
@@ -8,6 +8,8 @@ pub async fn create_adaptor() -> datastore_adaptor::DatastoreAdaptor {
     datastore_adaptor::DatastoreAdaptor::new().await
 }
 
+#[cfg(not(feature = "sql-adaptor"))]
+#[cfg(not(feature = "datastore-adaptor"))]
 pub async fn create_adaptor() -> memory_adaptor::MemoryAdaptor {
     memory_adaptor::MemoryAdaptor::new().await
 }
